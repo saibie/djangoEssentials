c46638d0b1f33532e749ab05890bdf53a828e062
saibie <saibie1677@gmail.com>
2024-03-08 13:23:53 +0900

    404 500 added

--- mainpage/views.py
+++ mainpage/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def mainView(request):
     return render(request, 'main.html')
+
+def page_not_found(request, exception):
+    return render(request, 'common/404.html', {})
+
+def internal_error(request, exception):
+    return render(request, 'common/500.html', {})

5d62f8b0a2fae3be565181b824024b1332a7ef9d
saibie <saibie1677@gmail.com>
2024-06-25 13:06:49 +0900

    Update prod.py

--- source/settings/prod.py
+++ source/settings/prod.py
@@ -1,6 +1,6 @@
 from source.settings import *
 
-# ALLOWED_HOSTS = ['ex.example.com']
+ALLOWED_HOSTS = ['127.0.0.1']
 DEBUG = False
 STATIC_ROOT = BASE_DIR / "static_open/"
 STATICFILES_DIRS = [BASE_DIR / "static/"]

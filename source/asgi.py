057d6484088da9de40cb42314f9f11000acf5860
saibie1677 <saibie1677@gmail.com>
2024-07-30 13:55:50 +0900

    websocket settings added

--- source/asgi.py
+++ source/asgi.py
@@ -11,6 +11,25 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+# Added for WebScoket -------------------------------------
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import source.websocket.routing
+# ---------------------------------------------------------
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "source.settings")
 
-application = get_asgi_application()
+# application = get_asgi_application()     # Default settings
+
+# Changed for WebSocket -----------------------------------
+application = ProtocolTypeRouter(
+    {
+        'http': get_asgi_application(),
+        'websocket': AuthMiddlewareStack(
+            URLRouter([
+                source.websocket.routing.websocket_urlpatterns
+            ])
+        ), 
+    }
+)
+# ---------------------------------------------------------
